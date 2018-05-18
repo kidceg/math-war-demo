c33488874bae1bf151b92040b6c647eb1254aa12
kidceg <861941721@qq.com>
2018-05-18 23:41:38 +0800

    add 6.0

---
+++ files/gakki/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w],x,_(y,z,A,B,C,D,E,_(),F,_(G,H,I,J,K,_(L,M,N,O),P,null,Q,J,R,J,S,T,U,null,V,W,X,Y,Z,ba,bb,W),bc,_(),bd,_(),be,_(bf,[])),bg,_(),bh,_());}; 
+var b="url",c="gakki.html",d="generationDate",e=new Date(1526657989675.61),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="phonePassword",o="ID",p="nicheng",q="answer",r="fName",s="setColor",t="signature",u="deleteFriend",v="addFriendName",w="addFriendPic",x="page",y="packageId",z="49c729a87a7a421eb31d356cfa13249b",A="type",B="Axure:Page",C="name",D="gakki",E="notes",F="style",G="baseStyle",H="627587b6038d43cca051c114ac41ad32",I="pageAlignment",J="near",K="fill",L="fillType",M="solid",N="color",O=0xFFFFFFFF,P="image",Q="imageHorizontalAlignment",R="imageVerticalAlignment",S="imageRepeat",T="auto",U="favicon",V="sketchFactor",W="0",X="colorStyle",Y="appliedColor",Z="fontName",ba="Applied Font",bb="borderWidth",bc="adaptiveStyles",bd="interactionMap",be="diagram",bf="objects",bg="masters",bh="objectPaths";
+return _creator();
+})());
